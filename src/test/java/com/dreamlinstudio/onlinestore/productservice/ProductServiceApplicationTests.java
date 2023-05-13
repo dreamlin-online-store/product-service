bf46eb68d0845b438f1aa1239d38070f003d0cde
Александр Слюсаренко <aleksandrslyusarenko@MacBook-Air-Aleksandr.local>
2023-05-13 12:24:36 +0200

    Initial commit

---
+++ src/test/java/com/dreamlinstudio/onlinestore/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamlinstudio.onlinestore.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
